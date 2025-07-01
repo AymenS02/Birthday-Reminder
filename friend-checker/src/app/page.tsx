2dd88bd3b86511bb24b66c88b95f0ec8f6039882
Aymen Shoteri <95427313+AymenS02@users.noreply.github.com>
2025-07-01 18:12:08 -0400

    set-up

--- friend-checker/src/app/page.tsx
+++ friend-checker/src/app/page.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Button } from '@/components/ui/button';
 import './globals.css';
+import { Button } from '@heroui/button';
+import { Motion } from "motion";
+
 export default function Home() {
   return (
     <div className="h-screen font-[family-name:var(--font-geist-sans)]">
@@ -12,14 +14,14 @@ export default function Home() {
           This app helps you keep track of your friends and the last time you spoke to them.
         </p>
 
-        <div className="mt-8">
-          <Button
-            href="/friends"
-            className="px-4 py-2 bg-background text-foreground rounded hover:bg-foreground hover:text-background transition-colors"
-          >
-            Go to Friends List
-          </Button>
-        </div>
+        <Motion.div
+          className="mt-8"
+          initial={{ opacity: 0, x: -50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <Button className="bg-amber-300" isDisabled size="sm">Click me</Button>
+        </Motion.div>
       </div>
     </div>
   );
